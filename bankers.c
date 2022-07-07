dfcc7c6ffbde9d6eec37c96e372c7a39724f135f
Pavan <96732038+Pavankulal2002@users.noreply.github.com>
2022-07-07 08:45:37 -0700

    Add files via upload

---
+++ bankers.c
@@ -0,0 +1,77 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main() {
+	int max[10][10],need[10][10],alloc[10][10],avail[10],completed[10],safesequence[10],p,r,i,j,process,count=0;
+
+	printf("Enter number of process :  ");
+	scanf("%d",&p);
+	for (i=0;i<p;i++)
+	completed[i]=0;
+
+	printf("Enter number of resource :  ");
+	scanf("%d",&r);
+
+	printf("Enter MAX matrix:");
+	for (i=0;i<p;i++) {
+		printf("\n For process %d :  ",i+1);
+		for (j=0;j<r;j++)
+		scanf("%d",&max[i][j]);
+	}
+
+	printf("\nEnter allocation:");
+	for (i=0;i<p;i++) {
+		printf("\n For process %d :  ",i+1);
+		for (j=0;j<r;j++)
+		scanf("%d",&alloc[i][j]);
+	}
+
+	printf("\nEnter available resource :  ");
+	for (i=0;i<r;i++)
+	scanf("%d",&avail[i]);
+	for (i=0;i<p;i++)
+	for (j=0;j<r;j++)
+	need[i][j]=max[i][j]-alloc[i][j];
+	do {
+		printf("\nMatrix:\tAllocation matrix:\n");
+		for (i=0;i<p;i++) {
+			for (j=0;j<r;j++)
+			printf("%d",max[i][j]);
+			printf("\t\t");
+			for (j=0;j<r;j++)
+			printf("%d",alloc[i][j]);
+			printf("\n");
+		}
+		process=-1;
+		for (i=0;i<p;i++) {
+			if(completed[i]==0) {
+				process=i;
+				for (j=0;j<r;j++) {
+					if(avail[j]<need[i][j]) {
+						process=-1;
+						break;
+					}
+				}
+			}
+			if(process!=-1)
+			break;
+		}
+		if(process!=-1) {
+			safesequence[count]=process+1;
+			count++;
+			for (j=0;j<r;j++) {
+				avail[j]+=alloc[process][j];
+				alloc[process][j]=0;
+				max[process][j]=0;
+				completed[process]=1;
+			}
+		}
+	}
+	while(count!=p&&process!=-1);
+	if(count==p) {
+		printf("\nThe system is in safe state\n");
+		printf("Safe sequence:");
+		for (i=0;i<p;i++)
+		printf("%d >",safesequence[i]);
+	} else
+	printf("\nThe system is in unsafe state!!\n");
+}
